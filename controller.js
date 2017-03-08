f966f78c51deaa1d87386a79d7808e8b4d2fa316
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-03-08 14:58:55 -0700

    Finished

--- controller.js
+++ controller.js
@@ -1,9 +1,27 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
+  $scope.currentPage = 1;
+
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	mainService.getUsers($scope.currentPage).then(function(response) {
+      $scope.users = response.data.data;
+    });
   }
 
+  $scope.prev = function() {
+    $scope.currentPage--;
+    if ($scope.currentPage < 1) {
+      $scope.currentPage = 1;
+    };
+    $scope.getUsers();
+  };
+  $scope.next = function() {
+    $scope.currentPage++;
+    if ($scope.currentPage > 4) {
+      $scope.currentPage = 4;
+    };
+    $scope.getUsers();
+  };
 
   $scope.getUsers();
 
